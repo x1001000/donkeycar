437009925ff3ddfd35a282ade8d28dfe8fc1107d
十百千 <x1001000@users.noreply.github.com>
2019-06-23 05:38:06 +0800

    Update mycontrol.py

--- mycontrol.py
+++ mycontrol.py
@@ -1,49 +1,47 @@
 import RPi.GPIO as GPIO
-L298N_In1=35
-L298N_In2=37
-L298N_In3=36
-L298N_In4=38
-L298N_En12=32
-L298N_En34=33
+
+L298N_EnA = 32
+L298N_EnB = 33
+L298N_In1 = 35
+L298N_In2 = 36
+L298N_In3 = 37
+L298N_In4 = 38
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(L298N_In1, GPIO.OUT)
 GPIO.setup(L298N_In2, GPIO.OUT)
 GPIO.setup(L298N_In3, GPIO.OUT)
 GPIO.setup(L298N_In4, GPIO.OUT)
-GPIO.setup(L298N_En12, GPIO.OUT)
-GPIO.setup(L298N_En34, GPIO.OUT)
+GPIO.setup(L298N_EnA, GPIO.OUT)
+GPIO.setup(L298N_EnB, GPIO.OUT)
 GPIO.output(L298N_In1, GPIO.HIGH)
 GPIO.output(L298N_In2, GPIO.LOW)
 GPIO.output(L298N_In3, GPIO.HIGH)
 GPIO.output(L298N_In4, GPIO.LOW)
-en12 = GPIO.PWM(L298N_En12, 123)
-en34 = GPIO.PWM(L298N_En34, 123)
-en12.start(0)
-en34.start(0)
+enL = GPIO.PWM(L298N_EnA, 1000)
+enR = GPIO.PWM(L298N_EnB, 1000)
+enL.start(0)
+enR.start(0)
+
 class control:
     def __init__(self):
         pass
     def run(self, angle, throttle):
-        #print('throttle:', round(throttle, 2))
-        #print('Moving!' if throttle > 0.5 else 'Stopped...')
         if throttle > 0:
-            dc12 = throttle * 90
-            dc34 = throttle * 100
-            if angle > 0:
-                dc34 *= (1 - angle)
-                print('dc34 =', dc34)
-            elif angle < 0:
-                dc12 *= (1 + angle)
-                print('dc12 =', dc12)
-            try:en12.ChangeDutyCycle(dc12)
-            except:pass
-            try:en34.ChangeDutyCycle(dc34)
-            except:pass
+            dcL = throttle * 100
+            dcR = throttle * 100
+            if angle < 0:
+                dcL *= (1 + angle)
+            else:
+                dcR *= (1 - angle)
+            print(dcL, dcR)
+            enL.ChangeDutyCycle(dcL)
+            enR.ChangeDutyCycle(dcR)
         else:
-            en12.ChangeDutyCycle(0)
-            en34.ChangeDutyCycle(0)
+            enL.ChangeDutyCycle(0)
+            enR.ChangeDutyCycle(0)
     def shutdown(self):
         #self.run(0, 0)
-        en12.stop()
-        en34.stop()
+        enL.stop()
+        enR.stop()
         GPIO.cleanup()
